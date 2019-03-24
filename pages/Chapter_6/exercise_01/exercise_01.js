f27e007b7d37b80d63a4efbcf10f0576b95aed78
tianwang <tianwang@buaa.edu.cn>
2019-03-24 22:11:31 +0800

    Chapter_6

---
+++ pages/Chapter_6/exercise_01/exercise_01.js
@@ -0,0 +1,12 @@
+// pages/Chapter_6/exercise_01/exercise_01.js
+Page({
+  data: {
+    iconSize: [20, 30, 40, 50, 60, 70],
+    iconColor: [
+      'red', 'orange', 'yellow', 'green', 'rgb(0,255,255)', 'blue', 'purple'
+    ],
+    iconType: [
+      'success', 'success_no_circle', 'info', 'warn', 'waiting', 'cancel', 'download', 'search', 'clear'
+    ]
+  }
+})
---
+++ pages/Chapter_6/exercise_02/exercise_02.js
@@ -0,0 +1,75 @@
+// pages/Chapter_6/exercise_02/exercise_02.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    text: "2011年1月，微信1.0发布,同年5月，微信2.0语音对讲发布,10月，微信3.0新增摇一摇功能,2012年3月，微信用户突破1亿,4月份，微信4.0朋友圈发布,同年7月，微信4.2发布公众平台,2013年8月，微信5.0发布微信支付,2014年9月，企业号发布,同月，发布微信卡包,2015年1月，微信第一条朋友圈广告,2016年1月，企业微信发布,2017年1月，小程序发布.",
+    htmlSnip: 
+    `<div class="div_class">
+      <h1>Bohemian Rhapsody</h1>
+      <h3>Director:Bryan Jay Singer</h3>
+      <img style="width:100%" src="https://img3.doubanio.com/view/photo/raw/public/p2522138824.jpg" />
+        <p style="color:#4b96bd;">
+          <b style="color:#000000">Intrduction: </b>Bohemian Rhapsody is a 2018 biographical film about <b>Freddie Mercury</b>, lead singer of the <i style="color:#f76260">British rock band Queen</i>. It follows the singer's life from when he joins the band in 1970 to their 1985 Live Aid performance at the original Wembley Stadium in London. 
+        </p>
+      </div>`
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
